8f6b729e05b0e76472150a39b6e56dff4ad80272
adonis vernalis <Adonis>
2024-02-05 23:56:48 +0600

    Initial commit

---
+++ dirSubDomain.py
@@ -0,0 +1,56 @@
+import requests
+import pyfiglet
+from termcolor import colored
+
+def check_subdomains(base_domain, wordlist_path):
+    with open(wordlist_path, 'r') as file:
+        sub_list = file.read().splitlines()
+
+    for sub in sub_list:
+        sub_domain = f"http://{sub}.{base_domain}"
+        try:
+            response = requests.get(sub_domain, timeout=3)
+            response.raise_for_status()
+            print("Valid domain:", sub_domain)
+        except requests.RequestException:
+            pass
+
+def check_directory(sub, directory):
+    full_url = f"http://{sub}/{directory}"
+    try:
+        response = requests.get(full_url)
+        response.raise_for_status()
+        print("Valid directory:", full_url)
+    except requests.exceptions.RequestException:
+        pass
+
+def print_welcome_message():
+    ascii_art = pyfiglet.figlet_format("dirSubDomain tool", font="slant")
+    print(colored(ascii_art, "red"))
+
+def main():
+    print_welcome_message()
+    mode = input("Choose mode subdomains[s] or directories[d]: ").lower()
+
+    if mode == "s":
+        base_domain = input("Your base domain:\n")
+        wordlist_path = input("Path to wordlist:\n")
+        check_subdomains(base_domain, wordlist_path)
+    elif mode == "d":
+        url = input("Your URL:\n")
+        wordlist_path = input("Path to wordlist:\n")
+
+        try:
+            with open(wordlist_path, "r") as file:
+                directories = file.read().splitlines()
+        except FileNotFoundError:
+            print(f"Error: Wordlist file '{wordlist_path}' not found.")
+            return
+
+        for directory in directories:
+            check_directory(url, directory)
+    else:
+        print("Invalid mode. Please choose 'subdomains' or 'directories'.")
+
+if __name__ == "__main__":
+    main()
